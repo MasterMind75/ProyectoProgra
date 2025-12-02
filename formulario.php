24d55e9b261241535ffbea5581f480a0ed019e96
MasterMind75 <ramirez.lugo.ethan.46@cbtis118.edu.mx>
2025-12-01 21:17:14 -0600

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -4,20 +4,18 @@
     <meta charset="UTF-8">
     <title>Libro de Visitas</title>
 </head>
-<body style="background-color: #1c1c1c; color: white; font-family: Arial;">
-    <h2>Libro de Visitas</h2>
+<body>
+    <h2>Deja tu comentario</h2>
 
     <form action="procesar.php" method="POST">
         <label>Nombre:</label><br>
         <input type="text" name="nombre" required><br><br>
 
         <label>Comentario:</label><br>
-        <textarea name="comentario" rows="4" required></textarea><br><br>
+        <textarea name="comentario" required></textarea><br><br>
 
         <button type="submit">Enviar</button>
     </form>
 
-    <br>
-    <button onclick="window.location.href='listado.php'">Ver Libro de Visitas</button>
 </body>
 </html>
