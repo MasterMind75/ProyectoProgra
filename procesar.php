5c6ef2d646b5e7cbae65e98fbfc9ecf9116ab165
MasterMind75 <ramirez.lugo.ethan.46@cbtis118.edu.mx>
2025-12-01 21:15:57 -0600

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -2,15 +2,17 @@
 $conexion = mysqli_connect("sql302.infinityfree.com","if0_40475193","Keyet123","if0_40475193_programacion");
 
 if (!$conexion) {
-    die("Error al conectar con la base de datos: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 
-$nombre = mysqli_real_escape_string($conexion, $_POST['nombre']);
-$comentario = mysqli_real_escape_string($conexion, $_POST['comentario']);
+$nombre = $_POST['nombre'];
+$comentario = $_POST['comentario'];
 
-$sql = "INSERT INTO libro_visitas (nombre, comentario)
-        VALUES ('$nombre', '$comentario')";
-mysqli_query($conexion, $sql);
+$query = "INSERT INTO visita (nombre, comentario) VALUES ('$nombre', '$comentario')";
+
+if (!mysqli_query($conexion, $query)) {
+    die("Error al insertar datos: " . mysqli_error($conexion));
+}
 
 mysqli_close($conexion);
 ?>
@@ -19,12 +21,14 @@ mysqli_close($conexion);
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Gracias por participar</title>
+    <title>Gracias</title>
 </head>
-<body style="background-color: #1c1c1c; color: white; font-family: Arial;">
+<body>
     <h1>¡Gracias por participar!</h1>
-    <button onclick="window.location.href='formulario.php'">Agregar otro comentario</button>
-    <button onclick="window.location.href='listado.php'">Ver Libro de Visitas</button>
-    <button onclick="window.location.href='TU_PAGINA_WIKI.html'">Regresar al Interactivo</button>
+    <p>Tu comentario fue guardado correctamente 🎉</p>
+
+    <a href="listado.php">
+        <button>Ver libro de visitas</button>
+    </a>
 </body>
 </html>
